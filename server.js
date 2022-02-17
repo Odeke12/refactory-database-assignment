57b055ba9420a372126180f7422165ebe2e0aa2d
Odeke12 <trevodex@gmail.com>
2022-02-18 00:10:49 +0300

    New server

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const db = mysql.createConnection({
 });
 var exists = false;
 var msg = "";
+
 //Connect
 db.connect((error) => {
 	if(error){
@@ -21,7 +22,11 @@ db.connect((error) => {
 	}
 });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());                                     
+app.use(express.urlencoded({extended: true}));               
+app.use(express.text());                                    
+app.use(express.json({ type: 'application/json'}));  
+// app.use(express.json)
 app.set("view engine", "ejs");
 
 //configurations or settings 
@@ -31,21 +36,24 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req,res) => {
 	// var exists = false;
 	res.render("form", {
-
-		check: false
+	check: false
 	});
+	
 }); 
 
 app.post("/", (req,res)=>{
 	let product = req.body;
 
-	console.log(req.body);
+	// console.log(req.body);
 	const sql1 = `SELECT * FROM students WHERE reg_no = "${req.body.reg_no}"`;
 	db.query(sql1, product, (err, result) => {
 		if(err) throw err;
 		console.log(result);
+		if(req.body.first_name == undefined && req.body.last_name == undefined && req.body.reg_no == undefined && req.body.age == undefined && req.body.class == undefined){
+			return("Please provide valid arguments")
+		}
 		if(result.length > 0){
-			console.log("Exists");
+			// return("Exists");
 			exists = true;
 			console.log(exists);
 			msg = "Registration number exists";
@@ -53,14 +61,16 @@ app.post("/", (req,res)=>{
 			const sql = "INSERT INTO students SET ?";
 			db.query(sql, product, (err, result) => {
 				if(err) throw err;
-				console.log(result);
+				// console.log(result);
 				
 			});
+			// res.json({message: "Student added!", student });
 		}
 		res.render("form", {
 			check: exists,
 			msg: msg
 		});
+		
 	});
 	
 
@@ -69,15 +79,67 @@ app.post("/", (req,res)=>{
 app.get("/students", (req,res) => {
 	const sql1 = "SELECT * FROM students";
 	db.query(sql1, (err, result) => {
+	// console.log(result)
 	if(err) throw err;
 	console.log(result);
 	res.render("students", {
+		msg:msg,
 		students: result,
-		check: false
+		check: exists
 	});
+	// res.status(200).send(result)
+	 return (result);
 	});
+	
 });
 
-app.listen("3000", () =>{
+app.get('*', (req, res) => {
+
+    res.status(404).send("Page not found")
+});
+
+app.post('/students/:id', (req,res) => {
+	console.log(req.params.id)
+	const sql1 = `SELECT * FROM students WHERE reg_no = "${req.body.reg_no}"`;
+	db.query(sql1, (err, result) => {
+		if(err) throw err;
+		console.log(result);
+		if(req.body.first_name == undefined && req.body.last_name == undefined && req.body.reg_no == undefined && req.body.age == undefined && req.body.class == undefined){
+			return("Please provide valid arguments")
+		}
+		if(result.length > 0){
+
+			exists = true;
+			console.log(exists);
+			msg = "Registration number exists";
+		}else{
+			const sql = `UPDATE students SET first_name="${req.body.fname}", last_name="${req.body.lname}", reg_no="${req.body.regno}", class="${req.body.class}", age =${req.body.age}  WHERE id=${req.params.id}`;
+			db.query(sql, (err, result) => {
+				if(err) throw err;
+	
+				msg = "Student successfully updated"
+			});
+			res.redirect('/students')
+		}
+		
+		
+	});
+})
+
+app.post('/delstudent/:id', (req,res) => {
+	const sql = `DELETE FROM students WHERE id= ${req.params.id}`
+	db.query(sql, (err, result) => {
+		console.log(result)
+		exists = true
+		msg = "Student successfully deleted"
+		res.redirect('/students')
+	})
+})
+
+const port = process.env.port || 3000
+
+app.listen(port, () =>{
 	console.log("Server running on port 3000");
 });
+
+module.exports = app
