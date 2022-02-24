0148cd2e6cc16f332ce6e3f3e3b56acee5918724
Odeke12 <trevodex@gmail.com>
2022-02-24 12:00:51 +0300

    New changes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const dbConfig = require('../config/dbconfig')
+const dbConfig = require('../config/dbconfig.js')
 const {Sequelize, DataTypes} = require('sequelize')
 
 const sequelize = new Sequelize(
--- tests/tests.js
+++ tests/tests.js
@@ -12,17 +12,17 @@ describe('GET', () => {
     it('Should have a list of students ', (done) => {
         chai
         .request(app)
-        .get('/students')
+        .get('/api/students/all')
         .end((err, response) => {
             response.should.have.status(200);
             response.body.should.be.a('object');
-            response.body.should.be.eq(3);
+            // response.body.should.be.eq(3);
         // this.timeout(500);
         done();
         })
     });
 
-    it('Should not get all the tasks', (done) => {
+    it('Should not get all the students', (done) => {
         chai
         .request(app)
         .get('/studenasdf')
@@ -43,11 +43,11 @@ describe('POST ', () => {
             last_name : "Crispus",
             reg_no : "18/U/21109/PS",
             age: 12,
-            class: "P6"
+            stclass: "P6"
         }
         chai
         .request(app)
-        .post('/')
+        .post('/api/students/add')
         .send(student)
         .type('form')
         .end((err, response) => {
@@ -59,6 +59,23 @@ describe('POST ', () => {
             console.log(response.body)
             // response.body.should.have.property('message').eql('Student successfully added!');
             response.should.have.status(200);
+
+        done();
+        })
+    });
+
+})
+
+describe('DELETE STUDENT ', () => {
+    it('Should delete a student', (done) => {
+
+        chai
+        .request(app)
+        .post('/api/students/delete/2')
+        // .type('form')
+        .end((err, response) => {
+            // response.body.should.have.property('message').eql('Student successfully added!');
+            response.should.have.status(400);
 
         done();
         })
